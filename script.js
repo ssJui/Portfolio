9ee87481ccdbe8d2caff912443595496fe821968
Sharmin Sultana Jui <ssjui.sust@gmail.com>
2026-02-18 11:19:18 +0600

    Initial commit

---
+++ script.js
@@ -0,0 +1,16 @@
+// Navbar shadow on scroll
+
+window.addEventListener("scroll", () => {
+  const nav = document.querySelector(".navbar");
+  if (window.scrollY > 50) {
+    nav.style.boxShadow = "0 5px 20px rgba(0,0,0,0.05)";
+  } else {
+    nav.style.boxShadow = "none";
+  }
+});
+
+document.querySelectorAll(".circle").forEach(circle => {
+  let percent = circle.getAttribute("data-percent");
+  let degree = percent * 3.6;
+  circle.style.background = `conic-gradient(#0ea5e9 ${degree}deg, #e5e7eb ${degree}deg)`;
+});
